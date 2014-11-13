11936e42a81e61aa51c8a7fdd1b2af100bea5875
pbjuhr <bjuhr.peter@gmail.com>
2014-11-13 23:19:20 +0100

    Added files

---
+++ src/FlashMessages_NoANAX.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace Pbjuhr\FlashMessages;
+ 
+/**
+ * Class for generating and displaying flash messages.
+ *
+ */
+class FlashMessages{
+
+    use \Anax\DI\TInjectable;
+
+
+
+    /**
+     * Contructor, sets di
+     */
+    public function __construct($di){
+		$this->di = $di;
+	}
+
+
+
+
+    /**
+     * Finds all Flashmessages from session
+     * @return all messages
+     */
+    public function findAll(){
+		return $this->session->get('FlashMessages', []);
+	}
+
+
+
+
+	/**
+	 * Adds a flash message to the session
+	 *
+	 * @param type (required), the type of message "info", "warning", "success" or "error".
+	 * @param content (required), the message (can contain html)
+	 *
+	 * @return void
+	 */
+	public function add($type, $content){
+
+		if($type != "warning" && $type != "success"  && $type != "error"){
+			$type = "info";
+		}
+
+		$messages = $this->findAll();
+
+		$messages[] = [
+			"type" => $type, 
+			"content" => $content,
+		];
+
+		$this->session->set('FlashMessages', $messages);
+
+	}
+
+
+
+	/**
+	 * Adds an info message using the add method.
+	 * @param $content (required), the message
+	 * @return void
+	 */
+	public function addInfo($content){
+		$this->add("info", $content);
+	}
+
+
+
+
+	/**
+	 * Adds a success message using the add method.
+	 * @param $content (required), the message
+	 * @return void
+	 */
+	public function addSuccess($content){
+		$this->add("success", $content);
+	}
+
+
+
+
+	/**
+	 * Adds a warning message using the add method.
+	 * @param $content (required), the message
+	 * @return void
+	 */
+	public function addWarning($content){
+		$this->add("warning", $content);
+	}
+
+
+
+
+	/**
+	 * Adds an error message using the add method.
+	 * @param $content (required), the message
+	 * @return void
+	 */
+	public function addError($content){
+		$this->add("error", $content);
+	}
+
+
+
+
+	/**
+	 * [getHtml description]
+	 * @param  $class (optional), sets a class to the div objects
+	 * @return $html, all messages formatted in html, or null if no messages exist
+	 */
+	public function getHtml($class = "alert"){
+
+        $messages = $this->findAll();
+		
+		/* if no messages exist */
+		if(count($messages) < 1)
+			return NULL;
+
+		$html = '';
+
+		foreach ($messages as $message){
+			if ( $message["type"] == "error")
+				$message["type"] = "danger"; 
+			$html .= '<div class="'. $class . ' '. $class . '-' .$message["type"].'" role="alert">'.$message["content"].'</div>';
+		}
+
+		$this->clean();
+
+		return $html;
+	}
+
+
+
+
+	/**
+	 * Resets the flash message session
+	 * @return void
+	 */
+	public function clean(){
+		$this->session->set('FlashMessages', NULL);
+	}
+
+}
