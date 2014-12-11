9b8777e0b2cb79d4325d68316a3d0f5048563ce7
pbjuhr <bjuhr.peter@gmail.com>
2014-12-11 21:28:21 +0100

    Added som tests

--- test/FlashMessages/FlashMessagesTest.php
+++ test/FlashMessages/FlashMessagesTest.php
@@ -13,10 +13,19 @@ class FlashMessagesTest extends \PHPUnit_Framework_TestCase {
 
 
     /**
-     * Creates an instance of the FlashMessage class
+     * Creates an instance of the FlashMessage class before every test
      */
-    public static function setUpBeforeClass() {
-        $this->$fm = new FlashMessages();
+    protected function setUp() {
+        $this->fm = new FlashMessages();
+    }
+
+
+
+    /**
+     * Resets the session
+     */
+    protected function tearDown() {
+        $_SESSION = null;
     }
 
 
@@ -25,8 +34,92 @@ class FlashMessagesTest extends \PHPUnit_Framework_TestCase {
     /**
      * Tests getSessionKey method
      */
-    public function testgetSessionKey() {
-        $this->assertEquals($fm->getSessionKey(), "FlashMessages", "Wrong session key.");
+    public function testGetSessionKey() {
+        $this->assertEquals($this->fm->getSessionKey(), "FlashMessages", "Wrong session key.");
+    }
+
+
+
+    /**
+     * Testing to add som messages
+     */
+    public function testAdd() {
+        $msg = "message";
+        $type = "invalid message type";
+        $msg2 = "message2";
+        $type2 = "warning";
+        $this->fm->add($msg, $type);
+        $this->fm->add($msg2, $type2);
+        $messages = $_SESSION[$this->fm->getSessionKey()];
+        $this->assertTrue(count($messages) === 2);
+        $this->assertEquals("info", $messages[0]["type"], "Wrong message type");
+        $this->assertEquals($msg, $messages[0]["content"], "Wrong message content");
+        $this->assertEquals($type2, $messages[1]["type"], "Wrong message2 type");
+        $this->assertEquals($msg2, $messages[1]["content"], "Wrong message2 content");
+    }
+
+
+
+
+    /**
+     * Tests clean method
+     * @depends testAdd
+     */
+    public function testClean() {
+        $this->fm->add("message", "error");
+    }
+
+
+
+
+    /**
+     * Testing to find all messages
+     */
+    public function testFindAll() {
+        $messages = $_SESSION[$this->fm->getSessionKey()];
+        $this->assertEquals([], $messages);
+        
+        $msg = "message";
+        $type = "invalid message type";
+        $msg2 = "message2";
+        $type2 = "warning";
+        $this->fm->add($msg, $type);
+        $this->fm->add($msg2, $type2);
+        $messages = $_SESSION[$this->fm->getSessionKey()];
+        $this->assertTrue(count($messages) === 2);
+        $this->assertEquals("info", $messages[0]["type"], "Wrong message type");
+        $this->assertEquals($msg, $messages[0]["content"], "Wrong message content");
+        $this->assertEquals($type2, $messages[1]["type"], "Wrong message2 type");
+        $this->assertEquals($msg2, $messages[1]["content"], "Wrong message2 content");
+    }
+    
+
+
+
+
+    /**
+     * Tests addError method
+     * @depends testAdd
+     */
+    public function testAddError() {
+        $msg = "message";
+        $this->fm->addError($msg);
+        $messages = $this->fm->findAll();
+        $this->assertTrue(count($messages) === 1);
+        $this->assertEquals("error", $messages[0]["type"], "Wrong message type");
+        $this->assertEquals($msg, $messages[0]["content"], "Wrong message content");
+    }
+
+
+
+
+    /**
+     * Tests changeSessionKey method
+     */
+    public function testChangeSessionKey() {
+        $newKey = "testkey";
+        $this->fm->changeSessionKey($newKey);
+        $this->assertEquals($this->fm->getSessionKey(), $newKey, "Wrong session key.");
     }
 
 }
